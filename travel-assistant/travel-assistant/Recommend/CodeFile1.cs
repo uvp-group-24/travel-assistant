be50d9632444f50fb888a18761d7609db7dcd860
杨尹豪 <1023523897@qq.com>
2017-04-07 20:51:11 +0800

    更新框架_4.7.1

---
+++ travel-assistant/travel-assistant/Recommend/CodeFile1.cs
@@ -0,0 +1,31 @@
+﻿//<Map:MapControl Name = "map"
+//                MapServiceToken="cXGdl8cifNITj1Mr19x6~duXenVSSPmR3wdc9dTzJ3w~AnzNY0HVZBSQ46JyDGq1WOHty5XUceOul4NeTDS_SQ5IhhdujACEG9Ay27Odu5pV" />
+
+//private async void click_Click(object sender, RoutedEventArgs e)
+//{
+//    // Set your current location.
+//    var accessStatus = await Geolocator.RequestAccessAsync();
+//    switch (accessStatus)
+//    {
+//        case GeolocationAccessStatus.Allowed:
+
+//            // Get the current location.
+//            Geolocator geolocator = new Geolocator();
+//            Geoposition pos = await geolocator.GetGeopositionAsync();
+//            Geopoint myLocation = pos.Coordinate.Point;
+
+//            // Set the map location.
+//            map.Center = myLocation;
+//            map.ZoomLevel = 12;
+//            map.LandmarksVisible = true;
+//            break;
+
+//        case GeolocationAccessStatus.Denied:
+//            // Handle the case  if access to location is denied.
+//            break;
+
+//        case GeolocationAccessStatus.Unspecified:
+//            // Handle the case if  an unspecified error occurs.
+//            break;
+//    }
+//}
--- travel-assistant/travel-assistant/Recommend/RecommendPage.xaml.cs
+++ travel-assistant/travel-assistant/Recommend/RecommendPage.xaml.cs
@@ -27,33 +27,5 @@ namespace travel_assistant.Recommend
         {
             this.InitializeComponent();
         }
-        private async void click_Click(object sender, RoutedEventArgs e)
-        {
-            // Set your current location.
-            var accessStatus = await Geolocator.RequestAccessAsync();
-            switch (accessStatus)
-            {
-                case GeolocationAccessStatus.Allowed:
-
-                    // Get the current location.
-                    Geolocator geolocator = new Geolocator();
-                    Geoposition pos = await geolocator.GetGeopositionAsync();
-                    Geopoint myLocation = pos.Coordinate.Point;
-
-                    // Set the map location.
-                    map.Center = myLocation;
-                    map.ZoomLevel = 12;
-                    map.LandmarksVisible = true;
-                    break;
-
-                case GeolocationAccessStatus.Denied:
-                    // Handle the case  if access to location is denied.
-                    break;
-
-                case GeolocationAccessStatus.Unspecified:
-                    // Handle the case if  an unspecified error occurs.
-                    break;
-            }
-        }
     }
 }
