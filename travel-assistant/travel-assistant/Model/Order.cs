1fdc01d8967b199866b7ef568f58a5d9c139c9ba
杨尹豪 <1023523897@qq.com>
2017-04-18 23:18:00 +0800

    基本视图v4.18.1

---
+++ travel-assistant/travel-assistant/Model/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travel_assistant.Model
+{
+    class Order
+    {
+        public string OrderNumber { get; set; } //订单号
+        public string UserID { get; set; } //用户ID
+        public string CouponName { get; set; } //券名
+        public int Quantity { get; set; } //购买数量
+        public double Costs { get; set; } //付款金额
+        public string PurchaseDate { get; set; } //购买日期
+        public OrderState State { get; set; } //订单状态
+    }
+    public enum OrderState
+    {
+        WaitForPayment,
+        Paid,
+        WaitForDelivery,
+        Delivering,
+        WaitForReception,
+        Received,
+        Consumed,
+        Refund
+    }
+}
---
+++ travel-assistant/travel-assistant/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using travel_assistant.Model;
+
+namespace travel_assistant.Model
+{
+    class User
+    {
+        public string Name { get; set; } //姓名
+        public double Wealth { get; set; } //账户余额
+        public List<Order> OrderList { get; set; } //订单列表
+    }
+}
